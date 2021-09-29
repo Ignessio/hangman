36b5291b74a079117ba2d95d5a56e1fc13364c58
valentin_zet <xerrox@yandex.ru>
2021-09-29 13:11:24 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Description:
+
+  The program outputs a greeting and shows the hidden letters in the random word, as well as the initial picture with the gallows.
+  The player must enter one letter at a step.
+  The letters that the player guesses are displayed in the hidden word.
+  Letters that are not in the word are displayed in the error list and new gallows pickture appears.
+  If the player has guessed all the letters, the game ends and the program congratulates the winner.
+  If the attempts are over, and the word is not opened, the player is considered as a looser.
+
+Program language: Ruby
+
+Run the program:
+  It is recommended to start the program by running main.rb file in terminal window.
+
+
+How change words to guess:
+  To change word to guess please edit \date\words.txt file
---
+++ main.rb
@@ -0,0 +1,33 @@
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+require_relative "lib/console_interface"
+require_relative "lib/game"
+
+puts "Всем привет!"
+
+# считываем случайную строку из файла и записываем её в переменную
+word = File.readlines("#{__dir__}/data/words.txt", chomp: true, encoding: 'utf-8').sample
+
+# создаем новый экземпляр игры и передаем ему слово в строке
+game = Game.new(word)
+# создаем новый экземпляр консольного интерфейса, которому передаем экземпляр игры
+console_interface = ConsoleInterface.new(game)
+
+# пока идет игра
+until game.over?
+  # командуем консольному интерфейсу, чтобы вывел текущее состояние
+  console_interface.print_out
+  # командуем консольному интерфейсу, чтобы запросил букву и записываем её в переменную
+  letter = console_interface.get_input
+  # передаем эту букву в переменной экземпляру игры и заставляем его запустить обсчет данных
+  game.play!(letter)
+end
+# когда всё кончено, командуем консольному интерфейсу вывести последние данные
+console_interface.print_out
